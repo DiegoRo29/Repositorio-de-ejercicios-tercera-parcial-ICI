1b5637bf7da90fdd85feba3a885d3d33ed3cef23
Juan Diego Rocha <111618195+DiegoRo29@users.noreply.github.com>
2022-12-01 20:07:18 -0600

    Add files via upload

---
+++ Importaciones/modsec.ex
@@ -0,0 +1,11 @@
+# Juan Diego Rocha Olivera
+#         3°B
+
+#Creamos el Módulo a importar modsec.ex
+#-Escribimos el siguiente código:
+
+defmodule ModuloImportado do
+  def funcion_importada(msg) do
+    IO.puts(msg)
+  end
+end
